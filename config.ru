6e0e6664d41d565bc123eeb1d3da2656e2164105
Matan Nahmani <matanmatan999new@gmail.com>
2020-01-22 15:10:27 +0200

    please work

--- config.ru
+++ config.ru
@@ -1,4 +1,5 @@
 require 'rubygems'
 require 'sinatra'
 require './search'
+require '/search'
 run Sinatra::Application
--- match_history.rb
+++ match_history.rb
@@ -5,6 +5,7 @@ require 'date'
 require "sinatra"
 require "sinatra/reloader" if development?
 require "better_errors"
+set :bind, '0.0.0.0'
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
